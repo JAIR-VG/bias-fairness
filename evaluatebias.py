8bb5fe4d7d53e32178d1ede1bd5ab933a0c953a4
JAIR-VG <vicente.jimenez@uacj.mx>
2024-07-16 19:34:53 -0600

    Cambio a otra base de datos

--- evaluatebias.py
+++ evaluatebias.py
@@ -3,13 +3,12 @@ sys.path.insert(1, "../")
 import numpy as np
 import pandas as pd
 
-from aif360.datasets import GermanDataset
+from aif360.datasets import AdultDataset
 
 np.random.seed(0)
 
-dataset_orig=GermanDataset()
+dataset_orig=AdultDataset()
 
 #print(dataset_orig)
 
 print(dataset_orig.feature_names)
-

8bd472f5aee2f582b45f9865457f21227b6bc7d1
JAIR-VG <vicente.jimenez@uacj.mx>
2024-07-23 00:05:31 -0600

    Airline Dataset

--- evaluatebias.py
+++ evaluatebias.py
@@ -2,13 +2,32 @@ import sys
 sys.path.insert(1, "../") 
 import numpy as np
 import pandas as pd
+import airlinedataset as aire
+#from aif360.datasets import AdultDataset
 
-from aif360.datasets import AdultDataset
 
 np.random.seed(0)
 
-dataset_orig=AdultDataset()
+dataset_orig=aire.AirlineDataset()
 
 #print(dataset_orig)
 
+
+print("#### Training Dataset shape")
+print(dataset_orig.features.shape,type(dataset_orig.features))
+
+print("#### Favorable and unfavorable labels")
+print(dataset_orig.favorable_label, dataset_orig.unfavorable_label)
+
+print("#### Protected attribute names")
+print(dataset_orig.protected_attribute_names)
+
+print("#### Privileged and unprivileged protected attribute values")
+print(dataset_orig.privileged_protected_attributes, 
+      dataset_orig.unprivileged_protected_attributes)
+
+
+
+print("#### Dataset feature names")
 print(dataset_orig.feature_names)
+print()

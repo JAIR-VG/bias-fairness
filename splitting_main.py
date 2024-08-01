a3a4f00e11af2e83d50c168559181c3623f67f37
JAIR-VG <vicente.jimenez@uacj.mx>
2024-07-31 21:51:02 -0600

    Final version of splitting utils

--- splitting_main.py
+++ splitting_main.py
@@ -3,79 +3,117 @@ import pandas as pd
 
 import splitting_utils as su
 
-from aif360.datasets import GermanDataset
+#from aif360.datasets import GermanDataset
+
+#from aif360.datasets import AdultDataset
+
+#from aif360.datasets import CompasDataset
+
+#from aif360.datasets import BankDataset
+#from aif360.datasets import MEPSDataset19
+#from aif360.datasets import MEPSDataset20
+from aif360.datasets import MEPSDataset21
 
 
+
+np.random.seed(1)
 
 dataset_m = GermanDataset()
-
+#dataset_m = AdultDataset()
+#dataset_m = CompasDataset()
+#dataset_m = BankDataset()
+#dataset_m = MEPSDataset19()
+#dataset_m = MEPSDataset20()
+#dataset_m = MEPSDataset21()
 privileged_groups = [{'sex': 1}]
 unprivileged_groups = [{'sex': 0}]
 
+#privileged_groups = [{'race': 1}]
+#unprivileged_groups = [{'race': 0}]
+#privileged_groups = [{'RACE': 1}]
+#unprivileged_groups = [{'RACE': 0}]
+#privileged_groups = [{'age': 1}]
+#unprivileged_groups = [{'age': 0}]
+
 sens_ind = 0
+
+
+
 
 ##print(dataset_m.feature_names)
 
-df = pd.DataFrame(dataset_m.features,columns=dataset_m.feature_names)
+#df = pd.DataFrame(dataset_m.features,columns=dataset_m.feature_names)
 #print(df.head())
 
 #Nombre de la columna/atributo protegido
-sens_attr = dataset_m.protected_attribute_names[sens_ind]
+#sens_attr = dataset_m.protected_attribute_names[sens_ind]
 
 #NOmbre de la columna/atributo de clase
-label_attr =dataset_m.label_names[0]
+#label_attr =dataset_m.label_names[0]
 
-print(sens_attr)
-print(label_attr)
+#print(sens_attr)
+#print(label_attr)
 #print(dataset_m.labels)
 
 
 #Etiquetas de Clase
-print(dataset_m.favorable_label)
-print(dataset_m.unfavorable_label)
+#print(dataset_m.favorable_label)
+#print(dataset_m.unfavorable_label)
 
-print(dataset_m.privileged_protected_attributes[sens_ind])
-print(dataset_m.unprivileged_protected_attributes[sens_ind])
+#print(dataset_m.privileged_protected_attributes[sens_ind])
+#print(dataset_m.unprivileged_protected_attributes[sens_ind])
 
-x=df[sens_attr].values.tolist()
-y=dataset_m.labels
-y=np.squeeze(y)
-y=list(y)
-#print(y)
-#print(x)
+#x=df[sens_attr].values.tolist()
+#y=dataset_m.labels
+#y=np.squeeze(y)
+#y=list(y)
 
-#print(df_two)
-
-print(type(x))
-print(type(y))
-
-print(x.count(0))
-print(x.count(1))
-
-#print(y[0])
-
-print(y.count(dataset_m.favorable_label))
-print(y.count(dataset_m.unfavorable_label))
-
-print(y[1])
- 
-lista = pd.DataFrame({'Protected':x, 'Etiqueta':y})
-
-print(lista)
-# display result  
-#print(y)
-
-#su.compute_feature_class(data_full,unprivileged_groups=unprivileged_groups,privileged_groups=privileged_groups)
-
-#print(data_full.features.shape)
-
-#print(data_full.labels)
-
-#print(data_full.features)
-#y=data_full.labels
-#x= data_full.features
 
 #print(type(x))
 #print(type(y))
-#print(y.shape)
-#print(y.ndim)
+
+#print(x.count(0))
+#print(x.count(1))
+
+#print(y[0])
+
+#print(y.count(dataset_m.favorable_label))
+#print(y.count(dataset_m.unfavorable_label))
+
+#print(y[1])
+ 
+#lista = pd.DataFrame({'Protected':x, 'Etiqueta':y})
+
+
+#new = lista.groupby(['Protected','Etiqueta']).size()
+
+new = su.compute_feature_class(dataset_m=dataset_m,
+                               unprivileged_groups=unprivileged_groups,
+                               privileged_groups=privileged_groups,
+                               sens_ind=sens_ind)
+
+print(new)
+
+print(type(new))
+
+df_train, df_test = dataset_m.split([0.8],shuffle=True)
+
+new = su.compute_feature_class(dataset_m=df_train,
+                               unprivileged_groups=unprivileged_groups,
+                               privileged_groups=privileged_groups,
+                               sens_ind=sens_ind)
+
+
+print(new)
+
+
+
+new = su.compute_feature_class(dataset_m=df_test,
+                               unprivileged_groups=unprivileged_groups,
+                               privileged_groups=privileged_groups,
+                               sens_ind=sens_ind)
+
+
+print(new)
+
+
--- splitting_utils.py
+++ splitting_utils.py
@@ -4,12 +4,47 @@ import numpy as np
 import pandas as pd
 
 
-def compute_feature_class(dataset_m,
-                          unprivileged_groups, privileged_groups):
+def compute_feature_class(dataset_m, unprivileged_groups, privileged_groups,sens_ind):
     
+    
+    keyp=list((privileged_groups[0]).keys())
+
+    valuep = list((privileged_groups[0]).values())
+    
+    valueunp = list((unprivileged_groups[0]).values())
+    
+    sens_attr =keyp[0]
+    #print(valuep[0])
+    #print(sens_attr)
+
+    valuep = valuep[0]
+    valueunp = valueunp[0]
+    
+
+    #Nombre de la columna/atributo protegido
+    #sens_attr = dataset_m.protected_attribute_names[sens_ind]
+
     df = pd.DataFrame(dataset_m.features,columns=dataset_m.feature_names)
-    print(df)
-    return df
+
+    x=df[sens_attr].values.tolist()
+    y=dataset_m.labels
+    y=np.squeeze(y)
+    y=list(y)
+    
+    
+    ncountp=x.count(valuep)
+    ncountunp =x.count(valueunp)
+
+    print('Number of Unprivileged Values =', ncountunp)
+    print('Number of Privileged =',ncountp)
+
+    lista = pd.DataFrame({'Protected':x, 'Etiqueta':y})
+    resume = lista.groupby(['Protected','Etiqueta']).size()
+
+    return resume
+
+    #print(df)
+    #return df
     #print(dataset_m.features.shape)
     #y=dataset_m.labels
     #x= dataset_m.features

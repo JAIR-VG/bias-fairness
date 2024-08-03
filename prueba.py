53fdacff2b61296bf2d00e89fc4b58e95ba40f27
JAIR-VG <vicente.jimenez@uacj.mx>
2024-08-03 17:25:55 -0600

    Performing double splitting

--- prueba.py
+++ prueba.py
@@ -10,10 +10,47 @@ dataset_m = GermanDataset()
 privileged_groups = [{'sex': 1}]
 unprivileged_groups = [{'sex': 0}]
 
-
 sens_ind = 0
 
-#su.double_splitting(dataset_m=dataset_m,
-#                    unprivileged_groups=unprivileged_groups,
-#                    privileged_groups=privileged_groups,
-#                    sens_ind=sens_ind)
+keyp=list((privileged_groups[sens_ind]).keys())
+
+print(keyp)
+
+valuep = list((privileged_groups[sens_ind]).values())
+
+print(valuep)
+
+valueunp = list((unprivileged_groups[sens_ind]).values())
+
+print(valueunp)
+
+#Obtiene los valores simples y no como una lista
+sens_attr =keyp[sens_ind]
+valuep = valuep[sens_ind]
+valueunp = valueunp[sens_ind]
+
+df = pd.DataFrame(dataset_m.features,columns=dataset_m.feature_names)
+
+print(df)
+
+x=df[sens_attr].values.tolist()
+
+#print(x)
+y=dataset_m.labels
+y=np.squeeze(y)
+y=list(y)
+#print(y)
+
+ncountp=x.count(valuep)
+ncountunp =x.count(valueunp)
+
+print('Number of Unprivileged Values =', ncountunp)
+print('Number of Privileged =',ncountp)
+
+df = pd.DataFrame({'Protected':x, 'Etiqueta':y})
+df['index'] = df.index
+print(df)
+
+lista=df.sort_values(['Protected','Etiqueta'])
+
+print(lista)

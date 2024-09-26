dbf87140f48bcf659a4f53e2a8ea24595eb689a4
JAIR-VG <vicente.jimenez@uacj.mx>
2024-09-26 00:23:38 -0600

    prueba

--- double_splitt_classification.py
+++ double_splitt_classification.py
@@ -13,11 +13,8 @@ from aif360.datasets import BankDataset
 from aif360.datasets import MEPSDataset19
 from aif360.datasets import CompasDataset
 
-from sklearn.linear_model import LogisticRegression as LG
-from sklearn.svm import SVC as SVM
-from sklearn.ensemble import RandomForestClassifier as RF
 
-"""
+
 dataset_orig = GermanDataset(
     protected_attribute_names=['age'],           # this dataset also contains protected
                                                  # attribute for "sex" which we do not
@@ -27,7 +24,7 @@ dataset_orig = GermanDataset(
 )
 privileged_groups = [{'age': 1}]
 unprivileged_groups = [{'age': 0}]
-"""
+
 
 """
 dataset_orig = GermanDataset(
@@ -78,32 +75,16 @@ privileged_groups = [{'sex': 1}]
 unprivileged_groups = [{'sex': 0}]
 dataset_orig = load_preproc_data_compas(['sex'])
 """
+"""
 privileged_groups = [{'race': 1}]
 unprivileged_groups = [{'race': 0}]
 dataset_orig = load_preproc_data_compas(['race'])
+"""
 
-dataset_orig_train, dataset_orig_test = dataset_orig.split([0.8], shuffle=True, seed=0)
-#print("#### Training Dataset shape")
-#print(dataset_orig.features.shape,type(dataset_orig.features))
-
-#print("#### Favorable and unfavorable labels")
-#print(dataset_orig.favorable_label, dataset_orig.unfavorable_label)
-
-#print("#### Protected attribute names")
-#print(dataset_orig.protected_attribute_names)
-
-#print("#### Privileged and unprivileged protected attribute values")
-#print(dataset_orig.privileged_protected_attributes, 
-#      dataset_orig.unprivileged_protected_attributes)
-
-#print("#### Dataset feature names")
-#print(dataset_orig.feature_names)
-#print()
 
 sens_ind = 0
 
-
-#resultado=SU.compute_distribution_feature(dm=dataset_orig,unprivileged_groups=unprivileged_groups,privileged_groups=privileged_groups)
+#Calculo sobre el conjunto original
 
 new = SU.compute_feature_class(dm=dataset_orig,
                                unprivileged_groups=unprivileged_groups,
@@ -112,36 +93,3 @@ new = SU.compute_feature_class(dm=dataset_orig,
 
 print(new)
 
-print(SU.compute_feature_class(dm=dataset_orig_train,
-                               unprivileged_groups=unprivileged_groups,
-                               privileged_groups=privileged_groups,
-                               sens_ind=sens_ind))
-
-print(SU.compute_feature_class(dm=dataset_orig_test,
-                               unprivileged_groups=unprivileged_groups,
-                               privileged_groups=privileged_groups,
-                               sens_ind=sens_ind))
-
-
-dataset_orig_train2,dataset_orig_test2 =SU.double_split(dm=dataset_orig,
-                                                        unprivileged_groups=unprivileged_groups,
-                                                        privileged_groups=privileged_groups,
-                                                        num_or_size_splits=[0.8],
-                                                        shuffle=True,seed=0)
-
-print(SU.compute_feature_class(dm=dataset_orig_train2,
-                               unprivileged_groups=unprivileged_groups,
-                               privileged_groups=privileged_groups,
-                               sens_ind=sens_ind))
-
-print(SU.compute_feature_class(dm=dataset_orig_test2,
-                               unprivileged_groups=unprivileged_groups,
-                               privileged_groups=privileged_groups,
-                               sens_ind=sens_ind))
-
-
-#df = pd.DataFrame(dataset_orig.features,columns=dataset_orig.feature_names)
-
-#print(df[['age']].to_string(index=False))
-
-#print(resultado)
--- splitting_main.py
+++ splitting_main.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 
-import splitt_utils as su
+import splitt_utils as mysplitt
+
+from sklearn.model_selection import StratifiedKFold
 
 from aif360.datasets import GermanDataset
 
@@ -18,105 +20,96 @@ from aif360.datasets import MEPSDataset21
 
 np.random.seed(1)
 
-#dataset_m = GermanDataset()
-#dataset_m = AdultDataset()
-#dataset_m = CompasDataset()
-#dataset_m = BankDataset()
-#dataset_m = MEPSDataset19()
-#dataset_m = MEPSDataset20()
-dataset_m = MEPSDataset21()
-#privileged_groups = [{'sex': 1}]
-#unprivileged_groups = [{'sex': 0}]
 
-#privileged_groups = [{'race': 1}]
-#unprivileged_groups = [{'race': 0}]
+dataset_orig = GermanDataset(
+    protected_attribute_names=['age'],           # this dataset also contains protected
+                                                 # attribute for "sex" which we do not
+                                                 # consider in this evaluation
+    privileged_classes=[lambda x: x >= 25],      # age >=25 is considered privileged
+    features_to_drop=['personal_status', 'sex'] # ignore sex-related attributes
+)
+privileged_groups = [{'age': 1}]
+unprivileged_groups = [{'age': 0}]
+
+
+"""
+dataset_orig = GermanDataset(
+    protected_attribute_names=['sex'],           # this dataset also contains protected
+                                                 # attribute for "sex" which we do not
+                                                 # consider in this evaluation
+    privileged_classes=[['male']],#,      # age >=25 is considered privileged
+    features_to_drop=['personal_status'] # ignore sex-related attributes
+)
+      
+privileged_groups = [{'sex': 1}]
+unprivileged_groups = [{'sex': 0}]
+"""
+"""
+dataset_orig = BankDataset()
+      
+privileged_groups = [{'age': 1}]
+unprivileged_groups = [{'age': 0}]
+
+"""
+"""
+dataset_orig = AdultDataset(protected_attribute_names=['sex'],
+                            privileged_classes=[['Male']],
+                            features_to_keep=['age', 'education-num'])
+
+privileged_groups = [{'sex': 1}]
+unprivileged_groups = [{'sex': 0}]
+"""
+"""
+dataset_orig = AdultDataset(protected_attribute_names=['race'],
+                            privileged_classes=[['White']],
+                            features_to_keep=['age', 'education-num'])
+
+privileged_groups = [{'race': 1}]
+unprivileged_groups = [{'race': 0}]
+"""
+"""
+dataset_orig = MEPSDataset19(protected_attribute_names=['RACE'],
+                            privileged_classes=[['White']])
+
+
+
 privileged_groups = [{'RACE': 1}]
 unprivileged_groups = [{'RACE': 0}]
-#privileged_groups = [{'age': 1}]
-#unprivileged_groups = [{'age': 0}]
+"""
+"""
+privileged_groups = [{'sex': 1}]
+unprivileged_groups = [{'sex': 0}]
+dataset_orig = load_preproc_data_compas(['sex'])
+"""
+"""
+privileged_groups = [{'race': 1}]
+unprivileged_groups = [{'race': 0}]
+dataset_orig = load_preproc_data_compas(['race'])
+"""
 
 sens_ind = 0
-
-
-
-
-##print(dataset_m.feature_names)
-
-#df = pd.DataFrame(dataset_m.features,columns=dataset_m.feature_names)
-#print(df.head())
-
-#Nombre de la columna/atributo protegido
-#sens_attr = dataset_m.protected_attribute_names[sens_ind]
-
-#NOmbre de la columna/atributo de clase
-#label_attr =dataset_m.label_names[0]
-
-#print(sens_attr)
-#print(label_attr)
-#print(dataset_m.labels)
-
-
-#Etiquetas de Clase
-#print(dataset_m.favorable_label)
-#print(dataset_m.unfavorable_label)
-
-#print(dataset_m.privileged_protected_attributes[sens_ind])
-#print(dataset_m.unprivileged_protected_attributes[sens_ind])
-
-#x=df[sens_attr].values.tolist()
-#y=dataset_m.labels
-#y=np.squeeze(y)
-#y=list(y)
-
-
-#print(type(x))
-#print(type(y))
-
-#print(x.count(0))
-#print(x.count(1))
-
-#print(y[0])
-
-#print(y.count(dataset_m.favorable_label))
-#print(y.count(dataset_m.unfavorable_label))
-
-#print(y[1])
- 
-#lista = pd.DataFrame({'Protected':x, 'Etiqueta':y})
-
-
-#new = lista.groupby(['Protected','Etiqueta']).size()
-
-new = su.compute_feature_class(dm=dataset_m,
+new = mysplitt.compute_feature_class(dm=dataset_orig,
                                unprivileged_groups=unprivileged_groups,
                                privileged_groups=privileged_groups,
                                sens_ind=sens_ind)
 
 print(new)
 
-resultado=su.compute_distribution_feature(dataset_m,unprivileged_groups=unprivileged_groups,privileged_groups=privileged_groups)
+skf=StratifiedKFold(n_splits=5,shuffle=True,random_state=0)
+X=dataset_orig.features
+y=dataset_orig.labels.ravel()
+for train_index, test_index in skf.split(X,y):
+    dtra,dtest = mysplitt.get_folds(dataset_orig,train_index,test_index)
+    print ("Fold Training")
+    newtra = mysplitt.compute_feature_class(dm=dtra,
+                                            unprivileged_groups=unprivileged_groups,
+                                            privileged_groups=privileged_groups,
+                                            sens_ind=sens_ind)
+    print(newtra)
 
-print(resultado)
-#print(type(new))
-
-df_train, df_test = dataset_m.split([0.8],shuffle=True)
-
-new = su.compute_feature_class(dm=df_train,
-                               unprivileged_groups=unprivileged_groups,
-                               privileged_groups=privileged_groups,
-                               sens_ind=sens_ind)
-
-
-print(new)
-
-
-
-new = su.compute_feature_class(dm=df_test,
-                               unprivileged_groups=unprivileged_groups,
-                               privileged_groups=privileged_groups,
-                               sens_ind=sens_ind)
-
-
-print(new)
-
-
+    newtst = mysplitt.compute_feature_class(dm=dtest,
+                                            unprivileged_groups=unprivileged_groups,
+                                            privileged_groups=privileged_groups,
+                                            sens_ind=sens_ind)
+    print ("Fold Test")
+    print(newtst)
